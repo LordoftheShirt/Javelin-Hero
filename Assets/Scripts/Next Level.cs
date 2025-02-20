0a5d88bf64de8edb0d607c6cdc5084d3b6c97788
LordoftheShirt <simon@polarcap.org>
2025-02-20 19:14:52 +0100

    small test
    had bad wifi when uploading. Want to make sure everything is up to date. Paranoia only

--- Assets/Scripts/Next Level.cs
+++ Assets/Scripts/Next Level.cs
@@ -7,6 +7,6 @@ public class NextLevel : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(0);
     }
 }
