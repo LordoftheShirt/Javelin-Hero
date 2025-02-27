c309cbccaeeb2500b8bf551bbce90e69a2fc3990
LordoftheShirt <102054756+LordoftheShirt@users.noreply.github.com>
2025-02-27 11:39:42 +0100

    Level2 constructed

--- Assets/Scripts/Next Level.cs
+++ Assets/Scripts/Next Level.cs
@@ -5,8 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class NextLevel : MonoBehaviour
 {
+    [SerializeField] private int goToLevel = 0;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(goToLevel);
     }
 }
