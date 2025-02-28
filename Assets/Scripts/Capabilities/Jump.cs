a801e558384ff6c94ede64f467e8daba80bc1ff7
LordoftheShirt <simon@polarcap.org>
2025-02-28 07:27:24 +0100

    made exportable

--- Assets/Scripts/Capabilities/Jump.cs
+++ Assets/Scripts/Capabilities/Jump.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.PackageManager.UI;
+//using UnityEditor.PackageManager.UI;
 using UnityEngine;
 
 public class Jump : MonoBehaviour
